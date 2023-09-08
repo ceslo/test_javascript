b5133eb3b0e0882e4ea0e80a042b18cfce79bc19
Sloves celine <sloves.ce@gmail.com>
2023-09-08 11:51:55 +0200

    phase 6 non corrigée

---
+++ phase_6.js
@@ -0,0 +1,11 @@
+var a="100";
+var b= 100 ;
+var c= 1.00 ;
+var d=true;
+var e= c+a
+var f=--b
+
+window.alert("Ceci est une chaîne de caractères: "+a);
+window.alert (f);
+window.alert (e);
+window.alert (!d)
