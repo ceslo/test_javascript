3749b3b35aed9a51500ef27d10a18f22b2298743
Sloves celine <sloves.ce@gmail.com>
2023-09-14 10:04:43 +0200

    phase 8ex4 non corrigé

---
+++ phase_8ex4.js
@@ -0,0 +1,11 @@
+var y=1;
+    var x = window.prompt("Entrez un nombre");
+    var n = window.prompt("Entrez le nombre de multiples de ce nombre à calculer");
+
+
+while (y<=n)
+{ 
+    
+    console.log (x+"x"+y+"="+(y*x));
+    ++y
+}
