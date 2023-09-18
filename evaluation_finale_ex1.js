91fd0fc0d646d77730cc5ef5ad718b457a72fef5
Sloves celine <sloves.ce@gmail.com>
2023-09-18 13:38:41 +0200

    ex1 eval 1er essai

---
+++ evaluation_finale_ex1.js
@@ -0,0 +1,20 @@
+var age;
+var x = 0;
+var y = 0;
+var z = 0;
+do  {
+    age = window.prompt("Entrez l'âge de la personne ici")
+    if (age < 20) {
+    var x = x + 1;
+  } else if (age >= 20 && age <= 40) {
+    var y = y + 1;
+  } else {
+    var z = z + 1;
+  }
+}
+  while (age < 100);
+
+
+  document.getElementById("part1").innerHTML="Le nombre de personne ayant strictement moins de 20 ans est "+x;
+  document.getElementById("part2").innerHTML="Le nombre de personne ayant entre 20 et 40 ans est "+y;
+  document.getElementById("part3").innerHTML="Le nombre de personne ayant strictement plus de 40 ans est "+z;
